eee10b898ab368e8961a1318ffd22ed483f243fe
aleks100s <aleksandr100s@yandex.ru>
2025-08-28 00:12:50 +0700

    Initial Commit

---
+++ Tripper/TripperApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripperApp.swift
+//  Tripper
+//
+//  Created by Alexander on 28.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct TripperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
